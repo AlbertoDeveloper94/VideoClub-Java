3c852adda8720c09257bac3cb36344ac7762a6cc
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-04-26 11:01:33 +0200

    feat: I create Movie Controller class

--- src/main/java/org/videoclub/controller/MovieController.java
+++ src/main/java/org/videoclub/controller/MovieController.java
@@ -1,4 +1,23 @@
 package org.videoclub.controller;
 
+import org.videoclub.model.Movie;
+import org.videoclub.repository.MovieRepository;
+
+import java.util.List;
+
 public class MovieController {
+
+    private final MovieRepository movieRepository;
+
+    public MovieController(MovieRepository movieService){
+        this.movieRepository = movieService;
+    }
+
+    public void createMovie(Movie movie) {
+        movieRepository.createMovie(movie);
+    }
+
+    public List<Movie> findAll() {
+        return movieRepository.findAll();
+    }
 }

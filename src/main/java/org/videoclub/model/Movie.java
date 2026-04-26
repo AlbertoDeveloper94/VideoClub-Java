16e33ffd04cc520a6977324625e04364ab383636
AlbertoDeveloper94 <bettogp94@gmail.com>
2026-04-26 10:54:07 +0200

    feat: I create Movie entity class to describe data info

--- src/main/java/org/videoclub/model/Movie.java
+++ src/main/java/org/videoclub/model/Movie.java
@@ -1,4 +1,85 @@
 package org.videoclub.model;
 
 public class Movie {
+
+    private int id;
+    private int year;
+    private int duration;
+
+    private String title;
+    private String director;
+    private String sinopsis;
+    private String language;
+    private String genre;
+
+    private Double rating;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
 }
